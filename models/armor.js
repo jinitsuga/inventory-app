1ac754528d85c438df94907576810368f2173dc7
agu <iba.agustin@gmail.com>
2023-02-27 16:26:58 -0300

    added model for gems

--- models/armor.js
+++ models/armor.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const ArmorSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  stock: { type: Number, required: true },
   description: { type: String, required: true },
   category: { type: Schema.Types.ObjectId, ref: "Category" },
   defense: { type: Number, required: true },
---
+++ models/gem.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GemSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  stock: { type: Number, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+});
+
+GemSchema.virtual("link").get(function () {
+  return `/shop/gem/${this._id}`;
+});

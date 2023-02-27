0c0370338fca3c0d11bdf758b728c6e235f7ba20
agu <iba.agustin@gmail.com>
2023-02-27 15:52:29 -0300

    created model files, added category model

---
+++ models/category.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+CategorySchema.virtual("link").get(function () {
+  return `/shop/categories/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", CategorySchema);

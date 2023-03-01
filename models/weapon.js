bbe0cd982973320ae2de734a03b0683f666afadf
agu <iba.agustin@gmail.com>
2023-03-01 14:26:59 -0300

    added weapon model/schema

---
+++ models/weapon.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WeaponSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  damage: { type: Number, required: true },
+  type: {
+    type: String,
+    required: true,
+    enum: ["Swords", "Axes", "Spears", "Daggers", "Bows", "Crossbows"],
+  },
+});
+
+WeaponSchema.virtual("link").get(function () {
+  return `/shop/weapons/${this._id}`;
+});
+
+module.exports = mongoose.model("Weapon", WeaponSchema);

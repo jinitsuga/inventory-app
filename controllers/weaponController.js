4247a1b9809527a05ade80c79e347f0ae5e2bfe0
agu <iba.agustin@gmail.com>
2023-03-12 15:56:21 -0300

    added controllers for displaying weapon edition form, and posting the update on the Database

--- controllers/weaponController.js
+++ controllers/weaponController.js
@@ -133,14 +133,80 @@ exports.weapon_delete_get = (req, res, next) => {
 };
 
 exports.weapon_delete_post = (req, res, next) => {
-  // emulates buying a wep (removes from shop)
-  res.send("deleting weapon from shop' database");
+  async.parallel(
+    {
+      weapon(callback) {
+        Weapon.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("weapon_delete", {
+        title: "Deleting weapon",
+        weapon: results.weapon,
+      });
+    }
+  );
 };
 
 exports.weapon_update_get = (req, res, next) => {
-  res.send("send form to update weapon");
+  async.parallel(
+    {
+      weapon(callback) {
+        Weapon.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      if (result.weapon == null) {
+        const error = new Error("weapon not found");
+        error.status = 404;
+        return next(error);
+      }
+      res.render("weapon_add", {
+        title: "Updating weapon info",
+        types: ["Swords", "Axes", "Spears", "Daggers", "Bows"],
+        typeError: "Weapon type not found",
+      });
+    }
+  );
 };
 
-exports.weapon_update_post = (req, res, next) => {
-  res.send("update weapon on shop's database");
-};
+exports.weapon_update_post = [
+  body("name", "Must include a name").trim().isLength({ min: 1 }).escape(),
+  body("price", "Please specify a price").trim().isLength({ min: 1 }).escape(),
+  body("stock", "Specify stock amount").trim().isLength({ min: 0 }).escape(),
+  body("description", "Please include item's description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("damage", "Specify weapon's damage")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("type", "Select a type").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const weapon = new Weapon({
+      name: req.body.name,
+      price: req.body.price,
+      stock: req.body.stock,
+      description: req.body.description,
+      damage: req.body.damage,
+      type: req.body.type,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("weapon_add", {
+        title: "Update weapon info",
+        types: ["Swords", "Axes", "Spears", "Daggers", "Bows"],
+        weapon: weapon,
+      });
+    }
+  },
+];
